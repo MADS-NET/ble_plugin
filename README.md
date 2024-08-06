d633ff1e5083d9377f4f424a52ef61ea2925101d
Paolo Bosetti <paolo.bosetti@unitn.it>
2024-08-06 12:46:51 +0200

    Support for event-based readings

--- README.md
+++ README.md
@@ -42,6 +42,25 @@ On Linux, the plugin requires the following packages to be installed:
 sudo apt install libdbus-1-dev
 ```
 
+## Services and Characteristics
+
+In BLE, data come organized in *services*, each service having one or more *characteristic*, i.e. individual value. The settings file must specify the peripheral name and a list of *characteristics* (an array of UUIDs). **If there are multiple characteristics under different services, this might cause issues and is not currently supported.**
+
+If the list of characteristics is empty (default), then all the characteristics are read.
+
+
+## Modes of operation
+
+It can operate in two modes, selected with the `subscribe` setting:
+
+1. **polling**: read the data published by the selected characteristics as soon as they are sent (default);
+2. **event**: get an immediate notification when a value is written by the device, typically only when it changes.
+
+
+## Arduino code
+
+The `arduino` folder contains a sketch suitable for testing purposes. It runs on arduino boards with BLE capabilities (as the Arduino R4). It produces readings for analog pins A1 and A2, suitable for polling mode, and for the digital pin D2, suitable for event-based notifications.
+
 
 ## INI settings
 
@@ -51,6 +70,8 @@ The plugin supports the following settings in the INI file:
 [ble_plugin]
 peripheral = "Arduino" # The name of the peripheral device
 characteristics = [] # a list of 128 bits valid UUID of the characteristics to be read
+subscribe = false    # subscribe to notifications (event mode)
+list_uuids = false   # on launch, print available UUIDs
 ```
 
 All settings are optional; if omitted, the default values are used.
--- arduino/ble/ble.ino
+++ arduino/ble/ble.ino
@@ -10,11 +10,12 @@ Logs analog inputs to BLE characteristics
 #include <ArduinoBLE.h>
 
 #define TIMESTEP 50 // time between readings in milliseconds
-
+#define THRESHOLD 100
 BLEService newService("180A"); // creating the service
 
-BLEUnsignedCharCharacteristic analog1("2A57", BLERead | BLENotify); // creating the Analog Value characteristic
-BLEUnsignedCharCharacteristic analog2("2A58", BLERead | BLENotify); // creating the Analog Value characteristic
+BLEUnsignedCharCharacteristic analog1("2A57", BLERead); // creating the Analog Value characteristic
+BLEUnsignedCharCharacteristic analog2("2A58", BLERead); // creating the Analog Value characteristic
+BLEBooleanCharacteristic pin2("2A59", BLERead | BLENotify);
 
 const int ledPin = 2;
 long previousMillis = 0;
@@ -28,6 +29,7 @@ void setup() {
 
   pinMode(LED_BUILTIN, OUTPUT); // initialize the built-in LED pin to indicate when a central is connected
   pinMode(ledPin, OUTPUT); // initialize the built-in LED pin to indicate when a central is connected
+  pinMode(2, INPUT_PULLDOWN);
 
   //initialize ArduinoBLE library
   if (!BLE.begin()) {
@@ -40,18 +42,19 @@ void setup() {
 
   newService.addCharacteristic(analog1); //add characteristics to a service
   newService.addCharacteristic(analog2);
+  newService.addCharacteristic(pin2);
 
   BLE.addService(newService);  // adding the service
 
   analog1.writeValue(0); //set initial value for characteristics
   analog2.writeValue(0);
+  pin2.writeValue(true);
 
   BLE.advertise(); //start advertising the service
   Serial.println(" Bluetooth® device active, waiting for connections...");
 }
 
 void loop() {
-  
   BLEDevice central = BLE.central(); // wait for a Bluetooth® Low Energy central
 
   if (central) {  // if a central is connected to the peripheral
@@ -63,14 +66,31 @@ void loop() {
 
     // check the battery level every TIMESTEP ms
     // while the central is connected:
+    static PinStatus pin2Value = HIGH;
     while (central.connected()) {
+      PinStatus v = digitalRead(D2);
       long currentMillis = millis();
       
+      // polling happens at TIMESTEP:
       if (currentMillis - previousMillis >= TIMESTEP) { 
         previousMillis = currentMillis;
+        int v1 = analogRead(A1);
+        int v2 = analogRead(A2);
+        if (v1 > THRESHOLD)
+          analog1.writeValue(v1);
+        else
+          analog1.writeValue(0);
+        if (v2 > THRESHOLD)
+          analog2.writeValue(v2);
+        else
+          analog2.writeValue(0);
+      }
 
-        analog1.writeValue(analogRead(A1));
-        analog2.writeValue(analogRead(A2));
+      // this runs at full speed, so the notification is immediate:
+      if (pin2Value != v) {
+        Serial.println(v ? "ON" : "OFF");
+        pin2.writeValue(v);
+        pin2Value = v;
       }
     }
     
